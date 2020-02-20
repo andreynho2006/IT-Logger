447f0ee0aed4e5351497e4a715e82840cde68c89
andreynho2006 <andreynho2006@gmail.com>
2020-02-20 23:12:14 +0200

    make a LogItem component and import it into Logs

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import LogItem from './LogItem';
 
 const Logs = () => {
   const [logs, setLogs] = useState([]);
@@ -21,18 +22,16 @@ const Logs = () => {
     return <h4>Loading</h4>;
   }
   return (
-    <div>
-      <ul className='collection-with-header'>
-        <li className='collection-header'>
-          <h4 className='center'>System logs</h4>
-        </li>
-        {!loading && logs.length === 0 ? (
-          <p className='center'>No logs to show</p>
-        ) : (
-          logs.map(log => <li>{log.message}</li>)
-        )}
-      </ul>
-    </div>
+    <ul className='collection with-header'>
+      <li className='collection-header'>
+        <h4 className='center'>System logs</h4>
+      </li>
+      {!loading && logs.length === 0 ? (
+        <p className='center'>No logs to show</p>
+      ) : (
+        logs.map(log => <LogItem log={log} key={log.id} />)
+      )}
+    </ul>
   );
 };
 
