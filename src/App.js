daa14191772660f4e60964ed9d9662eb4f645d33
andreynho2006 <andreynho2006@gmail.com>
2020-02-21 20:09:33 +0200

    make a TechListModal with all the techs in json-server db

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Logs from './components/logs/Logs';
 import AddBtn from './components/layout/AddBtn';
 import AddLogModal from './components/logs/AddLogModal';
 import EditLogModal from './components/logs/EditLogModal';
-import AddTechMoidal from './components/techs/AddTechModal';
+import AddTechModal from './components/techs/AddTechModal';
+import TechListModal from './components/techs/TechListModal';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
@@ -22,7 +23,8 @@ const App = () => {
         <AddBtn />
         <AddLogModal />
         <EditLogModal />
-        <AddTechMoidal />
+        <AddTechModal />
+        <TechListModal />
         <Logs />
       </div>
     </Fragment>
