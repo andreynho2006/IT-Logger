e08e38ea21107a2e247d9afde762c4ea21b4e0c6
andreynho2006 <andreynho2006@gmail.com>
2020-02-22 15:31:49 +0200

    add TechSelectOptions to add and edit logs

--- src/components/logs/AddLogModal.js
+++ src/components/logs/AddLogModal.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
+import TechSelecOptions from '../techs/TechSelectOptions';
 import PropTypes from 'prop-types';
 import { addLog } from '../../actions/logActions';
 import M from 'materialize-css/dist/js/materialize.min.js';
@@ -59,9 +60,7 @@ const AddLogModal = ({ addLog }) => {
               <option value='' disabled>
                 Select Technician
               </option>
-              <option value='Jon Doe'>Jon Doe</option>
-              <option value='Sam Smith'>Sam Smith</option>
-              <option value='Sara Wilson'>Sara Wilson</option>
+              <TechSelecOptions />
             </select>
           </div>
         </div>
--- src/components/logs/EditLogModal.js
+++ src/components/logs/EditLogModal.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
+import TechSelecOptions from '../techs/TechSelectOptions';
 import PropTypes from 'prop-types';
 import { updateLog } from '../../actions/logActions';
 import M from 'materialize-css/dist/js/materialize.min.js';
@@ -64,9 +65,7 @@ const EditLogModal = ({ updateLog, current }) => {
               <option value='' disabled>
                 Select Technician
               </option>
-              <option value='Jon Doe'>Jon Doe</option>
-              <option value='Sam Smith'>Sam Smith</option>
-              <option value='Sara Wilson'>Sara Wilson</option>
+              <TechSelecOptions />
             </select>
           </div>
         </div>
