c03271616cbde780144de773a182596e5ca70a2c
andreynho2006 <andreynho2006@gmail.com>
2020-02-22 10:37:23 +0200

    add DELETE_LOGS action

--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -1,4 +1,4 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR } from '../actions/types';
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG } from '../actions/types';
 
 const initialState = {
   logs: null,
@@ -12,7 +12,13 @@ export default (state = initialState, action) => {
     case GET_LOGS:
       return {
         ...state,
-        loags: action.payload,
+        logs: action.payload,
+        loading: false
+      };
+    case ADD_LOG:
+      return {
+        ...state,
+        logs: [...state.logs, action.payload],
         loading: false
       };
     case LOGS_ERROR:
