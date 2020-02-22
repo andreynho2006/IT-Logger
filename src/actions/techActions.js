b69dff113ee2ee9ecd575609b2bdd8b2adca81b2
andreynho2006 <andreynho2006@gmail.com>
2020-02-22 15:42:40 +0200

    call addTech method when a new tech is added

--- src/actions/techActions.js
+++ src/actions/techActions.js
@@ -7,7 +7,6 @@ import {
 } from './types';
 
 // get techs from server
-// get logs from server
 export const getTechs = () => async dispatch => {
   try {
     setLoading();
@@ -26,7 +25,31 @@ export const getTechs = () => async dispatch => {
   }
 };
 
-// set loading to true
+// add techs to server
+export const addTech = tech => async dispatch => {
+  try {
+    setLoading();
+    const res = await fetch('/techs', {
+      method: 'POST',
+      body: JSON.stringify(tech),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const data = await res.json();
+
+    dispatch({
+      type: ADD_TECH,
+      payload: data
+    });
+  } catch (err) {
+    dispatch({
+      type: TECHS_ERROR,
+      payload: err.response.statusText
+    });
+  }
+};
+
 // set loading to true
 export const setLoading = () => {
   return {
